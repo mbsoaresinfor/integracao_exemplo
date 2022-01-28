5b2b8659c799a5f2b6c89db31dcc15fac383ffb3
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2022-01-28 11:45:36 -0300

    conti. impll.

--- modulo-produto/src/main/java/br/com/mbs/Main.java
+++ modulo-produto/src/main/java/br/com/mbs/Main.java
@@ -2,11 +2,10 @@ package br.com.mbs;
 
 
 import org.springframework.boot.SpringApplication;
-
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
 
 @SpringBootApplication
 @EnableFeignClients
--- modulo-produto/src/main/java/br/com/mbs/repositorio/EstoqueRepositorio.java
+++ modulo-produto/src/main/java/br/com/mbs/repositorio/EstoqueRepositorio.java
@@ -1,14 +1,16 @@
 package br.com.mbs.repositorio;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 // url: endereco de onde esta o servico.
-@FeignClient(value = "modulo-estoque", url = "http://localhost:9091")
+@FeignClient(value = "modulo-estoque", url = "http://localhost:9091",fallback = Fallback.class)
 public interface EstoqueRepositorio {
 
 	 @RequestMapping(value = "/verificar-estoque", method = RequestMethod.GET, produces="application/json")	 
@@ -18,4 +20,20 @@ public interface EstoqueRepositorio {
 	 @RequestMapping(value = "/atualizar-estoque", method = RequestMethod.GET, produces="application/json")	 
 	  public ResponseEntity<Boolean>  atualizarEstoque(@RequestParam("idProduto")  Float idProduto,@RequestParam("quantidade") Integer quantidade) ;		 
 		 
+}
+
+@Component class Fallback implements EstoqueRepositorio {
+
+	@Override
+	public ResponseEntity<Boolean> verificarEstoque(Float idProduto, Integer quantidade) {
+
+		return ResponseEntity.ok(true);
+	}
+
+	@Override
+	public ResponseEntity<Boolean> atualizarEstoque(Float idProduto, Integer quantidade) {
+		 return ResponseEntity.ok(true);
+	}
+
+    
 }
